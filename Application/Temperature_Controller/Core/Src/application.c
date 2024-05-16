e2b24dc85c70c8a15fd03d10d3460d2a74c5a308
Pedro4064 <pedrohlcruz@gmail.com>
2024-05-16 03:55:04 -0300

    [IMPLEMENTED] Heater and Cooler PWM test

--- Application/Temperature_Controller/Core/Src/application.c
+++ Application/Temperature_Controller/Core/Src/application.c
@@ -13,21 +13,93 @@
 #include <usart.h>
 #include <tim.h>
 
-#include "application.h"
-#include "heaterAndCooler.h"
 #include "pwmConfig.h"
+#include "application.h"
+#include "buttonsEvents.h"
+#include "heaterAndCooler.h"
 
+// Application Specific Macros 
+#define PWM_DUTYCYCLE_INCREMENT(x, delta) x+delta > 1 ? 1 : x+delta
+#define PWM_DUTYCYCLE_DECREMENT(x, delta) x-delta < 0 ? 0 : x-delta
+
+// Application Test Variables
+float fCoolerDutyCycle = 0;
+float fHeaterDutyCycle = 0;
+
+// Config Settings 
 pwmConfig xHeaterConfig = {&htim1, TIM_CHANNEL_1};
 pwmConfig xCoolerConfig = {&htim8, TIM_CHANNEL_1};
+
+ButtonMapping xBoardButtons[] = {
+                                        {BTN_UP_GPIO_Port,    BTN_UP_EXTI_IRQn, BTN_UP_Pin},
+                                        {BTN_DOWN_GPIO_Port,  BTN_DOWN_EXTI_IRQn, BTN_DOWN_Pin},
+                                        {BTN_LEFT_GPIO_Port,  BTN_LEFT_EXTI_IRQn, BTN_LEFT_Pin},
+                                        {BTN_RIGHT_GPIO_Port, BTN_RIGHT_EXTI_IRQn, BTN_RIGHT_Pin},
+                                        {BTN_ENTER_GPIO_Port, BTN_ENTER_EXTI_IRQn, BTN_ENTER_Pin}
+                                    };
+
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef* pTimer){
+
+	if(pTimer->Instance == TIM7 || pTimer->Instance == TIM16)
+		vButtonsEventsTimerCallback(pTimer);
+
+}
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
+
+    vButtonsEventsGpioCallback(GPIO_Pin);
+
+}
+
+void vApplicationButtonPressed(Button xPressedButton){
+	if (xPressedButton == UP)
+		fCoolerDutyCycle = PWM_DUTYCYCLE_INCREMENT(fCoolerDutyCycle, 0.1);
+
+	else if (xPressedButton == DOWN)
+		fCoolerDutyCycle = PWM_DUTYCYCLE_DECREMENT(fCoolerDutyCycle, 0.1);
+
+	else if (xPressedButton == RIGHT)
+		fHeaterDutyCycle = PWM_DUTYCYCLE_INCREMENT(fHeaterDutyCycle, 0.1);
+
+	else if (xPressedButton == LEFT)
+		fHeaterDutyCycle = PWM_DUTYCYCLE_DECREMENT(fHeaterDutyCycle, 0.1);
+
+
+	vHeaterSetPwmDuty(fHeaterDutyCycle);
+	vCoolerFanSetPwmDuty(fCoolerDutyCycle);
+}
+
+void vApplicationButtonReleased(Button xPressedButton){}
+
+void vApplicationButtonThreeSecondPressed(Button xPressedButton){}
+
+void vApplicationButtonHalfSecondPressed(Button xPressedButton){
+
+	if (xPressedButton == UP)
+		fCoolerDutyCycle = PWM_DUTYCYCLE_INCREMENT(fCoolerDutyCycle, 0.2);
+
+	else if (xPressedButton == DOWN)
+		fCoolerDutyCycle = PWM_DUTYCYCLE_DECREMENT(fCoolerDutyCycle, 0.2);
+
+	else if (xPressedButton == RIGHT)
+		fHeaterDutyCycle = PWM_DUTYCYCLE_INCREMENT(fHeaterDutyCycle, 0.2);
+
+	else if (xPressedButton == LEFT)
+		fHeaterDutyCycle = PWM_DUTYCYCLE_DECREMENT(fHeaterDutyCycle, 0.2);
+
+	vHeaterSetPwmDuty(fHeaterDutyCycle);
+	vCoolerFanSetPwmDuty(fCoolerDutyCycle);
+}
 
 void vApplicationStart() {
 
 	heaterAndCoolerInit(&xHeaterConfig, &xCoolerConfig);
-	vHeaterSetPwmDuty(0.5);
-	vCoolerFanSetPwmDuty(0.3);
-
 	vHeaterStart();
 	vCoolerStart();
+
+	vButtonsEventsInit(&xBoardButtons, &htim7, &htim16, &vApplicationButtonPressed, &vApplicationButtonReleased, &vApplicationButtonHalfSecondPressed, &vApplicationButtonThreeSecondPressed);
+
 
     while (1) {
 
