034c7a13ed4c99b8b82087102757a34c11e27e6c
Pedro4064 <pedrohlcruz@gmail.com>
2024-05-24 16:21:39 -0300

    [IMPLEMENTED] Doc strings

--- Application/Temperature_Controller/Core/Inc/tachometer.h
+++ Application/Temperature_Controller/Core/Inc/tachometer.h
@@ -5,9 +5,51 @@
 
 extern float fTachometerMeasuredSpeed;
 
+// ********************************************************** //
+// Method name:        vTachometerInit                        //
+// Method description: Initialize  all  the necessary timers  //
+//                     for the tachometer operation           //
+// Input params:       pEnableTim                             //
+//                        Pointer   to   the   timer   handle //
+//                        responsible  by  the  enable signal //
+//                        (the  tachometer  uses fixed window //
+//                        frequency counting)                 //
+//                     pCounterTim                            //
+//                        Pointer   to   the   timer   handle //
+//                        responsible  for the counter update //
+//                        (for  the  fixed  window  frequency //
+//                        counter algorithm)                  //
+//                     uiEnablePeriod                         //
+//                        Period, in ms, of the enable period //
+// Output params:      void                                   //
+//                        N/A                                 //
+// ********************************************************** //
 void vTachometerInit(TIM_HandleTypeDef* pEnableTim, TIM_HandleTypeDef* pCounterTim, unsigned int uiEnablePeriod);
 
+// ********************************************************** //
+// Method name:        vTachometerStartReadings               //
+// Method description: Start  the  tachometer  readings  by   //
+//                     starting the enable timer (the value   //
+//                     shall  be  calculate  on  the  timer   //
+//                     callback function)                     //
+// Input params:       void                                   //
+//                        N/A                                 //
+// Output params:      void                                   //
+//                        N/A                                 //
+// ********************************************************** //
 void vTachometerStartReadings(void);
 
+// ********************************************************** //
+// Method name:        vTachometerUpdateSpeed                 //
+// Method description: Calculate the speed from the captured  //
+//                     pulses      and      update      the   //
+//                     fTachometerMeasuredSpeed   variable.   //
+//                     This function shall be called in the   //
+//                     timer callback                         //
+// Input params:       void                                   //
+//                        N/A                                 //
+// Output params:      void                                   //
+//                        N/A                                 //
+// ********************************************************** //
 void vTachometerUpdateSpeed(void);
 #endif
