73db64b2de1c50c54adc98fa1787a59c3ff2abba
Pedro4064 <pedrohlcruz@gmail.com>
2024-06-16 22:05:06 -0300

    [IMPLEMENTED] Queue for handling series of async events

---
+++ Application/Temperature_Controller/Core/Inc/queue.h
@@ -0,0 +1,18 @@
+#ifndef H_QUEUE_INC_
+#define H_QUEUE_INC_
+
+typedef struct QUEUE{
+    char cQ[10];
+    int iTop;
+    int iBottom;
+    int iSize;
+} queue;
+
+
+void vQueueInsert(queue* pQ, char cValue);
+
+char cQueueGet(queue* pQ);
+
+char cQueueIsEmpty(queue* pQ);
+
+#endif
---
+++ Application/Temperature_Controller/Core/Src/queue.c
@@ -0,0 +1,43 @@
+#include "queue.h"
+
+#define BUFFER_SIZE 10
+#define CIRCULAR_INCREMENT(x) (x+1)%BUFFER_SIZE
+#define CIRCULAR_DECREMENT(x) ((x - 1) < 0)? (BUFFER_SIZE - 1):x-1
+#define ELEMENT_COUNTER_INCREMENT(x) (x+1) > BUFFER_SIZE? BUFFER_SIZE:x+1
+#define ELEMENT_COUNTER_DECREMENT(x) (x-1) < 0? 0:x-1
+
+void vQueueInsert(queue* pQ, char cValue){
+    
+    // If the queue is empty, do not increment iBottom and iTop, since the first element 
+    //is being inserted the top and bottom elements are the same, and incrementing
+    //would cause an error in the cQueueGet logic
+    pQ->iBottom = cQueueIsEmpty(pQ) ?  pQ->iBottom:CIRCULAR_INCREMENT(pQ->iBottom) ;
+    pQ->iTop = (pQ->iBottom == pQ->iTop && !cQueueIsEmpty(pQ)) ? CIRCULAR_INCREMENT(pQ->iTop): pQ->iTop;
+
+    pQ->cQ[pQ->iBottom] = cValue;
+
+    
+    pQ->iSize = ELEMENT_COUNTER_INCREMENT(pQ->iSize);
+}
+
+char cQueueGet(queue* pQ){
+
+    // If deQueue operation is done on an empty queue, return zero
+    if(cQueueIsEmpty(pQ))
+        return 0;
+
+    // Decrement the element counter to account for the element being removed
+    pQ->iSize = ELEMENT_COUNTER_DECREMENT(pQ->iSize);
+
+    // If There are no more elements, there is no need to increment the Top Counter,
+    //since doing so would cause a mismatch between the Top and Bottom indexes and
+    //would result in an error on the vQueueInsert logic
+    char cReturnValue = pQ->cQ[pQ->iTop];
+    pQ->iTop = cQueueIsEmpty(pQ) ? pQ-> iTop : CIRCULAR_INCREMENT(pQ->iTop);
+    
+    return cReturnValue;
+}
+
+char cQueueIsEmpty(queue* pQ){
+    return pQ->iSize == 0;
+}
