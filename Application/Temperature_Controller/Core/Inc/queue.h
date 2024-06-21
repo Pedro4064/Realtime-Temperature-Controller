514435bf81c41c3881947bfc821eed5b543f768a
Pedro4064 <pedrohlcruz@gmail.com>
2024-06-20 21:44:11 -0300

    [IMPLEMENTED] Doc string for queue library

--- Application/Temperature_Controller/Core/Inc/queue.h
+++ Application/Temperature_Controller/Core/Inc/queue.h
@@ -9,11 +9,47 @@ typedef struct QUEUE{
 } queue;
 
 
+// ********************************************************** //
+// Method name:        vQueueInsert                           //
+// Method description: Insert element in Queue                //
+// Input params:       pQ                                     //
+//                        Pointer  to the queue to insert the //
+//                        value                               //
+//                     cValue                                 //
+//                        Value to be inserted into the queue //
+// Output params:      void                                   //
+//                        N/A                                 //
+// ********************************************************** //
 void vQueueInsert(queue* pQ, char cValue);
 
+// ********************************************************** //
+// Method name:        cQueueGet                              //
+// Method description: Get the first element in the Queue     //
+// Input params:       pQ                                     //
+//                        Pointer to the queue                //
+// Output params:      char                                   //
+//                        First value in the queue (FIFO)     //
+// ********************************************************** //
 char cQueueGet(queue* pQ);
 
+// ********************************************************** //
+// Method name:        cQueueIsEmpty                          //
+// Method description: Check if queue is empty                //
+// Input params:       pQ                                     //
+//                        Pointer to the queue                //
+// Output params:      char                                   //
+//                        Boolean  value,  1  if  is  empty 0 //
+//                        otherwise                           //
+// ********************************************************** //
 char cQueueIsEmpty(queue* pQ);
 
+// ********************************************************** //
+// Method name:        vQueueClear                            //
+// Method description: Clear the queue                        //
+// Input params:       pQ                                     //
+//                        Pointer to the queue to be cleared  //
+// Output params:      void                                   //
+//                        N/A                                 //
+// ********************************************************** //
 void vQueueClear(queue* pQ);
 #endif
