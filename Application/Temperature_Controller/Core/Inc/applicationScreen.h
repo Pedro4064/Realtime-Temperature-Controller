48c2a8e38106b6919c83940020e5cc4779c7c4e4
Pedro4064 <pedrohlcruz@gmail.com>
2024-06-20 21:15:28 -0300

    [IMPLEMENTED] Doc string for applicationScreen sub-system

--- Application/Temperature_Controller/Core/Inc/applicationScreen.h
+++ Application/Temperature_Controller/Core/Inc/applicationScreen.h
@@ -4,8 +4,33 @@
 #ifndef INC_APPLICATION_SCREEN_H_
 #define INC_APPLICATION_SCREEN_H_
 
+// ********************************************************** //
+// Method name:        vApplicationScreenInit                 //
+// Method description: Initialize  the application`s Screen   //
+//                     Service                                //
+// Input params:       pLcdConfiguration                      //
+//                        Pointer to the struct containing the//
+//                        configurations necessary for driving//
+//                        the LCD display                     //
+//                     pAppParam                              //
+//                        Pointer    to   the   application's //
+//                        parameters,   used   to  both  read //
+//                        information  to  be  displayed  and //
+//                        request    actions    from    other //
+//                        sub-systems                         //
+// Output params:      void                                   //
+//                        N/A                                 //
+// ********************************************************** //
 void vApplicationScreenInit(lcdConfig* pLcdConfiguration, applicationParameters* pAppParam);
 
+// ********************************************************** //
+// Method name:        vApplicationScreenUpdate               //
+// Method description: Update the Screen states               //
+// Input params:       void                                   //
+//                        N/A                                 //
+// Output params:      void                                   //
+//                        N/A                                 //
+// ********************************************************** //
 void vApplicationScreenUpdate();
 
 #endif
