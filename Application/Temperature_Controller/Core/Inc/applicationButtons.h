f951af049a7401534039baa5cc50549b06857e6e
Pedro4064 <pedrohlcruz@gmail.com>
2024-06-20 21:24:06 -0300

    [IMPLEMENTED] Doc string for application Buttons interface

--- Application/Temperature_Controller/Core/Inc/applicationButtons.h
+++ Application/Temperature_Controller/Core/Inc/applicationButtons.h
@@ -6,6 +6,28 @@
 #include "application.h"
 
 
+// ********************************************************** //
+// Method name:        vApplicationButtonsInit                //
+// Method description: Initialize  the application's button   //
+//                     interface                              //
+// Input params:       pBoardButtonMapping                    //
+//                        Pointer  to  struct  containing all //
+//                        button mapping information          //
+//                     pDebounceTim                           //
+//                        Pointer to timer handle responsible //
+//                        for debounce implementation         //
+//                     pLongPressTim                          //
+//                        Pointer to timer handle responsible //
+//                        for  long  press  time tracking and //
+//                        callback trigger                    //
+//                     pApplicationParam                      //
+//                        Pointer to application's parameters,//
+//                        used  to  trigger  actions on other //
+//                        sub-systems  from  press  and  long //
+//                        press callbacks                     //
+// Output params:      void                                   //
+//                        N/A                                 //
+// ********************************************************** //
 void vApplicationButtonsInit(ButtonMapping (*pBoardButtonMapping)[NUMBER_BOARD_BUTTONS],TIM_HandleTypeDef* pDebounceTim, TIM_HandleTypeDef* pLongPressTim, applicationParameters* pApplicationParam);
 
 #endif
